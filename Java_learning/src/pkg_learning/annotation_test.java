e1a45b29eb4770ab2666a7a9bfce62ea94b99e58
boseutsav <utsav050489@gmail.com>
2021-08-06 10:59:22 +0530

    aa

--- Java_learning/src/pkg_learning/annotation_test.java
+++ Java_learning/src/pkg_learning/annotation_test.java
@@ -11,8 +11,6 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-
-
 public class annotation_test {
 	@Test(priority=2)
 	public void test1() {
