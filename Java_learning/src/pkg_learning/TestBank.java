7ae3a87ce8567446316ed5f64898bf0d3ed2328c
boseutsav <utsav050489@gmail.com>
2026-03-01 16:09:21 +0530

    new changes

--- Java_learning/src/pkg_learning/TestBank.java
+++ Java_learning/src/pkg_learning/TestBank.java
@@ -6,6 +6,12 @@ public class TestBank {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Bank obj = new Bank();
+		Bank obj1 = new Bank_ABC();
+		Bank_ABC obj2 = (Bank_ABC) obj1;
+		System.out.println(obj.equals(obj1));
+		System.out.println(obj==obj1);
+		System.out.println(obj2.equals(obj1));
+		System.out.println(obj2==obj1);
 
 	}
 }
